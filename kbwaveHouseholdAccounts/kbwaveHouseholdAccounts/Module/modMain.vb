8277b947e3c13ea479b644aa514b41b1ca54dda1
kbwave <kbwavemain@gmail.com>
2019-01-02 07:36:12 +0900

    Add Project
    Creates Household Accounts Project

---
+++ kbwaveHouseholdAccounts/kbwaveHouseholdAccounts/Module/modMain.vb
@@ -0,0 +1,19 @@
+﻿''' <summary>
+''' 本アプリのメインモジュール
+''' </summary>
+''' <remarks></remarks>
+Module modMain
+
+	Private _windowMain As windowMain = Nothing
+
+
+	<STAThread> _
+	Public Sub Main()
+
+
+
+	End Sub
+
+
+
+End Module
---
+++ kbwaveHouseholdAccounts/kbwaveHouseholdAccounts/Window/windowMain.vb
@@ -0,0 +1,3 @@
+﻿Public Class windowMain
+
+End Class
